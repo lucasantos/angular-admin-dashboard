7a27e635a793d73ee576737b167cf799b30f6df1
Lucas Santos <lucas@lucasantos.com>
2026-05-12 19:37:55 -0300

    Implementing mock API and DB with json-server, add proxy config to prevent CORS, add concurrently to run the application together the API(just to development without real API)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.yourdomain.com/api',
+  mockAPI: 'http://localhost:3000', // URL do json-server para dados de teste
   VAPID_PUBLIC_KEY: 'YOUR_PUBLIC_VAPID_KEY', // Replace with your actual VAPID public key
 };
