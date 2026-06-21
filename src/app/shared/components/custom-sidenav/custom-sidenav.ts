0efa305fbbdcaa6588dc192e605805b8ba806b54
Lucas Santos <lucas@lucasantos.com>
2026-06-20 22:23:42 -0300

    Improve UI/UX from the sidebar

--- src/app/shared/components/custom-sidenav/custom-sidenav.ts
+++ src/app/shared/components/custom-sidenav/custom-sidenav.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, Input, signal } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from "@angular/material/list";
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -11,26 +11,26 @@ import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-custom-sidenav',
-  imports: [CommonModule, MatListModule, MatIconModule, RouterModule, MatTooltipModule, MenuItem, TranslocoPipe],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatIconModule,
+    RouterModule,
+    MatTooltipModule,
+    MenuItem,
+    TranslocoPipe,
+  ],
   templateUrl: './custom-sidenav.html',
   styleUrl: './custom-sidenav.scss',
 })
 export class CustomSidenav {
   protected readonly authService = inject<AuthService>(AuthService);
-  sideNavCollapsed = signal(false);
 
-  @Input() set collapsed(value: boolean) {
-    this.sideNavCollapsed.set(value);
-  }
+  collapsed = input.required<boolean>();
+  menuItems = input.required<MenuItems[]>();
 
-  @Input() set menuItems(value: MenuItems[]) {
-    this._menuItems.set(value);
-  }
+  protected readonly sideNavCollapsed = computed(() => this.collapsed());
+  protected readonly items = computed(() => this.menuItems());
 
-  private readonly _menuItems = signal<MenuItems[]>([]);
-
-  // Expose menu items as readonly signal for template
-  protected readonly items = this._menuItems.asReadonly();
-
-  profilePicSize = computed(() => (this.sideNavCollapsed() ? '50' : '100'));
+  protected readonly profilePicSize = 100;
 }
